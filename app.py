b2ea0a0390e0d7355e20100143ddcadac308188c
Amna Noor <amnanoor5778@gmail.com>
2025-12-19 17:41:04 +0500

    add routes and pages

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/mood")
+def mood():     
+    return render_template("mood.html") 
+
+@app.route("/report")
+def report():
+    return render_template("report.html")
